290379213debd9019738285cf9568cfe9ce06938
shivam <shivamduhanx@gmail.com>
2023-06-28 18:07:07 -0400

    Update Top Interview 046 - #219 (Contains Duplicate II).c

--- Top Interview 046 - #219 (Contains Duplicate II).c
+++ Top Interview 046 - #219 (Contains Duplicate II).c
@@ -7,31 +7,29 @@ typedef struct Entry {
 typedef struct HashTable {
     Entry** buckets;
     int num_buckets;
-} HashTable; 
+} HashTable; //hash table is an array of buckets where each bucket holds frequencies
 
 HashTable* hash_table_create(int num_buckets) {
     HashTable* hash_table = malloc(sizeof(HashTable));
-    hash_table->buckets = calloc(num_buckets, sizeof(Entry*));
+    hash_table->buckets = calloc(num_buckets, sizeof(Entry*)); //init the buckets
     hash_table->num_buckets = num_buckets;
-    return hash_table;
+    return hash_table; //hash table created
 }
 
 Entry* hash_table_get(HashTable* hash_table, int key) {
-    int bucket_index = abs(key) % hash_table->num_buckets;
+    int bucket_index = abs(key) % hash_table->num_buckets; //hash to get which bucket to search
     Entry* entry = hash_table->buckets[bucket_index];
-    while (entry != NULL && entry->key != key) {
-        entry = entry->next;
-    }
+    while (entry != NULL && entry->key != key) {entry = entry->next;} //traverse linked list at that index
     return entry;
 }
 
 void hash_table_put(HashTable* hash_table, int key, int value) {
-    int bucket_index = abs(key) % hash_table->num_buckets;
-    Entry* existing_entry = hash_table_get(hash_table, key);
+    int bucket_index = abs(key) % hash_table->num_buckets; //hash to get which bucket to store val
+    Entry* existing_entry = hash_table_get(hash_table, key); //get existing val at key to check collisions
     if (existing_entry != NULL) {
-        existing_entry->value = value;
+        existing_entry->value = value; //no collisions, just add
     } else {
-        Entry* new_entry = malloc(sizeof(Entry));
+        Entry* new_entry = malloc(sizeof(Entry)); //collision, add node at beginning of linked list
         new_entry->key = key;
         new_entry->value = value;
         new_entry->next = hash_table->buckets[bucket_index];
@@ -48,8 +46,8 @@ void hash_table_destroy(HashTable* hash_table) {
             entry = next_entry;
         }
     }
-    free(hash_table->buckets);
-    free(hash_table);
+    free(hash_table->buckets); //deallocate memory for buckets
+    free(hash_table); //deallocate memory for table
 }
 
 bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
