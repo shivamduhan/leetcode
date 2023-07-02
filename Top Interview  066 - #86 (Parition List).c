e5c4b24cb864a15b52d8c96f78268cedf22d0ba4
shivam <shivamduhanx@gmail.com>
2023-07-02 19:18:01 -0400

    Create Top Interview  066 - #86 (Parition List).c

---
+++ Top Interview  066 - #86 (Parition List).c
@@ -0,0 +1,20 @@
+struct ListNode* partition(struct ListNode* head, int x){
+    struct ListNode * smallHead = (struct ListNode*)malloc(sizeof(struct ListNode)); //preserve head of smaller list
+    struct ListNode * smallerList = smallHead;
+    struct ListNode * bigHead = (struct ListNode*) malloc(sizeof(struct ListNode)); //preserve head of larger list
+    struct ListNode * bigList = bigHead;
+    while (head) {
+        if (head -> val < x){
+            smallerList->next = head; //append node to before list
+            smallerList = smallerList->next; 
+        }
+        else {
+            bigList->next = head; //append node to after list
+            bigList = bigList->next; 
+        }
+        head = head->next;
+    }
+    bigList->next = NULL; //avoid cycle
+    smallerList->next=bigHead->next; //connect partitions
+    return smallHead->next;
+}
