c81a6706c063e8dc28998bc0a0027da02c884cad
shivam <shivamduhanx@gmail.com>
2023-07-14 15:46:34 -0400

    Update Top Interview 080 - #236 (Lowest Common Ancestor of a Binary Tree).c

--- Top Interview 080 - #236 (Lowest Common Ancestor of a Binary Tree).c
+++ Top Interview 080 - #236 (Lowest Common Ancestor of a Binary Tree).c
@@ -4,5 +4,5 @@ struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p,
     struct TreeNode * right = lowestCommonAncestor(root->right, p, q); //post order traversal (recurse right and left)
     struct TreeNode * left = lowestCommonAncestor(root->left, p, q);
     if (left && right) {return root;} //one node found in left subtree, other in right subtree
-    return left ? left : right;    //either left or right null i.e. both in 1 subtree
+    return left ? left : right;    //either left or right null i.e. both in 1 subtree so return what is not NULL
 }
