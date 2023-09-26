c804b8f2f2bd428c3954c10c8574c88d3731219a
shivam <shivamduhanx@gmail.com>
2023-09-26 00:27:44 -0400

    Update Top Interview some - #83 (Remove duplicates from sorted list).c

--- Top Interview some - #83 (Remove duplicates from sorted list).c
+++ Top Interview some - #83 (Remove duplicates from sorted list).c
@@ -1,3 +1,4 @@
+/*
 struct ListNode* deleteDuplicates(struct ListNode* head){
     struct ListNode* curr = head;
     while (curr) {
@@ -5,6 +6,20 @@ struct ListNode* deleteDuplicates(struct ListNode* head){
             curr -> next = curr->next->next;            
         }
         curr = curr->next;        
+    }
+    return head;
+}
+*/
+
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    if (head == NULL) {return NULL;}
+    struct ListNode* curr = head;    
+    while (curr -> next) {
+        if (curr->val == curr->next->val) {
+            struct ListNode*tmp = curr->next;
+            curr -> next = curr->next->next;
+            free(tmp);
+        } else {curr = curr->next;}        
     }
     return head;
 }
